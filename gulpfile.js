a6859da191369c7ea6fac845bf277d435947c25a
nickp10 <nickp10@hotmail.com>
2018-02-26 15:20:10 -0700

    Fix project setup to support unit tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const argv = require("argv");
 const babel = require("gulp-babel");
 const gulp = require("gulp");
+const mocha = require("gulp-mocha");
 const path = require("path");
 const sourcemaps = require("gulp-sourcemaps");
 const typescript = require("gulp-typescript");
@@ -10,13 +11,13 @@ const args = argv.option({ name: "env", short: "e", type: "string" }).run();
 const isDebug = args.options["env"] === "debug";
 const destDirname = isDebug ? "debug" : "build";
 const dest = `./${destDirname}`;
-const tsconfig = typescript("tsconfig.json");
+const tsconfig = () => typescript("tsconfig.json");
 
 gulp.task("compile", () => {
-    const src = gulp.src(["src/**/*.ts", "!src/**/*.d.ts"], { base: "./src" });
+    const src = gulp.src(["src/**/*.ts", "!src/**/*.d.ts", "!src/**/*.spec.ts"], { base: "./src" });
     if (isDebug) {
         return src.pipe(sourcemaps.init())
-            .pipe(tsconfig)
+            .pipe(tsconfig())
             .pipe(sourcemaps.mapSources((sourcePath, file) => {
                 const to = path.dirname(file.path);
                 const buildToRoot = path.relative(to, __dirname);
@@ -27,13 +28,33 @@ gulp.task("compile", () => {
             .pipe(sourcemaps.write(""))
             .pipe(gulp.dest(dest));
     } else {
-        return src.pipe(tsconfig)
+        return src.pipe(tsconfig())
             .pipe(babel({
                 presets: ["env"]
             }))
             .pipe(uglify())
             .pipe(gulp.dest(dest));
     }
+});
+
+gulp.task("compile-test", () => {
+    return gulp.src(["src/**/*.spec.ts"], { base: "./src" })
+        .pipe(sourcemaps.init())
+        .pipe(tsconfig())
+        .pipe(sourcemaps.mapSources((sourcePath, file) => {
+            const to = path.dirname(file.path);
+            const buildToRoot = path.relative(to, __dirname);
+            const rootToSource = path.relative(__dirname, to);
+            const fileName = path.basename(sourcePath);
+            return path.join(buildToRoot, rootToSource, fileName);
+        }))
+        .pipe(sourcemaps.write(""))
+        .pipe(gulp.dest(dest));
+});
+
+gulp.task("test", ["compile", "compile-test"], () => {
+    return gulp.src([`${destDirname}/**/*.spec.js`])
+        .pipe(mocha());
 });
 
 gulp.task("build", ["compile"]);
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,14 @@ import * as args from "./args";
 import * as express from "express";
 
 const app = express();
+app.get("/", (req, res) => {
+    res.sendStatus(200);
+})
+app.get("/:contestId/:playerId", (req, res) => {
+    const contestId = req.params.contestId;
+    const playerId = req.params.playerId;
+    res.sendStatus(200);
+});
 app.listen(args.port, () => {
     console.log(`Server has started on port ${args.port}`);
 });
